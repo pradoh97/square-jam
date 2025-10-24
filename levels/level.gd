7c5c0e3c5b3e15a1c0f1d28c8bed9558e69539f2
pradoh97 <pradoh97@gmail.com>
2025-10-24 18:55:26 -0300

    Add camera limits

--- levels/level.gd
+++ levels/level.gd
@@ -3,18 +3,27 @@ class_name Level
 # Nice values: 50, 70, 110
 @export var bpm: float = 70.0
 @export var movement_step: int = 64
+@export var screen_left_limit: int = -576
+@export var screen_right_limit: int = 1125
 var blacks_duration: float = 0.0
 
+
 func _ready():
+	$Player/Camera2D.limit_enabled = true
+	$Player/Camera2D.limit_left = screen_left_limit
+	$Player/Camera2D.limit_right = screen_right_limit
 	var seconds_in_minute = 60
 	blacks_duration = seconds_in_minute/bpm
 	$Blacks.wait_time = blacks_duration
 
-	$CompassFeedback.set_blacks_duration(blacks_duration)
-	$Blacks.timeout.connect($CompassFeedback._on_blacks_timeout)
+	$UI/CompassFeedback.set_blacks_duration(blacks_duration)
+	$Blacks.timeout.connect($UI/CompassFeedback._on_blacks_timeout)
 
 	$Grid.draw(movement_step)
 
 	for hazard: Hazard in $Hazards.get_children():
 		hazard.blacks_duration = blacks_duration
 		$Blacks.timeout.connect(hazard._on_blacks_timeout)
+		hazard.set_timers()
+	for check_point: CheckPoint in $CheckPoints.get_children():
+		check_point.body_entered.connect(func(_body): $Player.respawn_position = check_point.global_position)
