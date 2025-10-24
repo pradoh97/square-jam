8d4d15c58e4d20dada9c3314e883c5941501f8fe
pradoh97 <pradoh97@gmail.com>
2025-10-24 18:54:36 -0300

    Add invert hazard movement

--- hazards/hazard.gd
+++ hazards/hazard.gd
@@ -4,20 +4,20 @@ class_name Hazard
 @export var movement_step = 64
 @export var forgiveness = 0.1
 @export var active = false
-@export var use_tween = true
-@export var required_beats = 1
+@export var downwards_movement = false
+@export var required_beats: float = 1.0
 
+var movement_direction = 1
 var blacks_duration = 1.0
 var current_beat = 0
 
 func _ready():
-	$ForgivenessAttackTimer.wait_time = blacks_duration*forgiveness
-	$ForgivenessAttackTimer.one_shot = true
+	if downwards_movement:
+		movement_direction = -1
 	$ForgivenessAttackTimer.timeout.connect(func(): $CollisionShape2D.set_deferred("disabled", false))
-	$ForgivenessReleaseTimer.wait_time = required_beats*blacks_duration - blacks_duration*forgiveness*required_beats
-	$ForgivenessReleaseTimer.one_shot = true
 	$ForgivenessReleaseTimer.timeout.connect(func(): $CollisionShape2D.set_deferred("disabled", true))
 	body_entered.connect(_on_body_entered)
+
 	if active:
 		$CollisionShape2D.set_deferred("disabled", false)
 	else:
@@ -26,26 +26,23 @@ func _ready():
 
 func move():
 	active = not active
-	var tween
-	if use_tween:
-		tween = create_tween()
+	var tween: Tween = create_tween()
 	if active:
-		if use_tween:
-			tween.set_ease(Tween.EASE_OUT)
-			tween.set_trans(Tween.TRANS_QUINT)
-			tween.tween_property(self, "global_position:y", global_position.y - movement_step, blacks_duration*0.25)
-		else:
-			global_position.y -= movement_step
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_QUINT)
+		tween.tween_property(self, "global_position:y", global_position.y - movement_direction*movement_step, blacks_duration*forgiveness)
 		$ForgivenessAttackTimer.start()
 		$CollisionShape2D.set_deferred("disabled", true)
 	elif not active:
-		if use_tween:
-			tween.set_ease(Tween.EASE_OUT)
-			tween.set_trans(Tween.TRANS_QUINT)
-			tween.tween_property(self, "global_position:y", global_position.y + movement_step, blacks_duration*0.25)
-		else:
-			global_position.y += movement_step
+		tween.set_ease(Tween.EASE_OUT)
+		tween.set_trans(Tween.TRANS_QUINT)
+		tween.tween_property(self, "global_position:y", global_position.y + movement_direction*movement_step, blacks_duration*forgiveness)
 
+func set_timers():
+	$ForgivenessAttackTimer.wait_time = blacks_duration*forgiveness
+	$ForgivenessAttackTimer.one_shot = true
+	$ForgivenessReleaseTimer.wait_time = required_beats*blacks_duration - blacks_duration*forgiveness*required_beats
+	$ForgivenessReleaseTimer.one_shot = true
 
 func _on_blacks_timeout():
 	current_beat += 1
