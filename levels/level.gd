c47a72bb343c979863c9b20fb20727d2b5a500e4
pradoh97 <pradoh97@gmail.com>
2025-10-27 00:22:23 -0300

    Add level end

--- levels/level.gd
+++ levels/level.gd
@@ -7,7 +7,7 @@ class_name Level
 var blacks_duration: float = 0.0
 
 func _ready():
-	$Grid.visible = true
+	%Grid.visible = true
 	$UI.visible = true
 	$Player/Camera2D.limit_enabled = true
 	$Player/Camera2D.limit_left = screen_left_limit
@@ -17,13 +17,19 @@ func _ready():
 
 	$UI/CompassFeedback.set_blacks_duration(blacks_duration)
 	$Blacks.timeout.connect($UI/CompassFeedback._on_blacks_timeout)
+	$Blacks.timeout.connect($Player._on_blacks_timeout)
 
-	$Grid.draw(movement_step)
+	%Grid.draw(movement_step)
 
 	for section: LevelSection in $LevelSections.get_children():
 		section.connect_hazards_to_beat(blacks_duration, $Blacks)
 		section.register_checkpoints($Player)
 	$Timer.timeout.connect(func(): %TimeElapsed.text = str(int(%TimeElapsed.text) + 1))
+	$Blacks.timeout.emit()
 
 func get_player() -> Player:
 	return $Player
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	get_tree().call_deferred("reload_current_scene")
